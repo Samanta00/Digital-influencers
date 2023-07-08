870c80c43d26745b14743c3ad5715a6186960a0a
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-08 13:31:30 -0300

    feat: principal page created

---
+++ front/src/app/Interfaces/page/page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.css']
+})
+export class PageComponent {
+  constructor(private router: Router) {}
+
+  irParaCadastro() {
+    // Lógica para navegar para a página de cadastro
+    this.router.navigate(['/register']);
+  }
+
+  irParaLogin() {
+    // Lógica para navegar para a página de login
+    this.router.navigate(['/login']);
+  }
+
+}
