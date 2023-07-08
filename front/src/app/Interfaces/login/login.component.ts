dc26cb4c1c61d51fe60b2787380eb2f803d616b9
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-08 17:01:42 -0300

    fix: interface login created

--- front/src/app/Interfaces/login/login.component.ts
+++ front/src/app/Interfaces/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      senha: ['', Validators.required]
+    });
+  }
+
+  fazerLogin() {
+    // Simulando a lógica de autenticação
+    const email = this.loginForm.controls['email'].value;
+    const senha = this.loginForm.controls['senha'].value;
+
+    if (email === 'exemplo@email.com' && senha === 'senha') {
+      // Redirecionar para a página desejada após o login
+      this.router.navigate(['/dashboard']);
+    } else {
+      // Lógica de tratamento de erro ou exibição de mensagem de login inválido
+    }
+  }
 }
