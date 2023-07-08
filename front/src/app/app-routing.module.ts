0655872f00880df57b9aac20582ac9f9054991ba
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-08 17:01:00 -0300

    fix: added new router to login

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageComponent } from './Interfaces/page/page.component';
 import { RegisterComponent } from './Interfaces/register/register.component';
+import { LoginComponent } from './Interfaces/login/login.component';
 
 const routes: Routes = [
   {path:'',component:PageComponent},
-  {path:'register', component:RegisterComponent}
+  {path:'register', component:RegisterComponent},
+  {path:'login', component:LoginComponent}
   
 ];
 
