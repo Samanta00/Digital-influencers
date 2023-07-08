99950ae2fb646ab6f4b6d44019b728fdc1809873
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-08 13:33:06 -0300

    fix: register design created

--- front/src/app/Interfaces/register/register.component.ts
+++ front/src/app/Interfaces/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  cadastroForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.cadastroForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      quantidadeInscritos: ['', Validators.required],
+      canal: ['', Validators.required],
+      plataforma: ['', Validators.required],
+      categoriaConteudo: ['', Validators.required]
+    });
+  }
+
+  cadastrarPessoa() {
+    if (this.cadastroForm.valid) {
+      // Aqui você pode fazer a lógica para enviar os dados do formulário para o servidor ou realizar outras ações
+      console.log(this.cadastroForm.value);
+    } else {
+      // Caso o formulário seja inválido, você pode exibir uma mensagem de erro ou realizar outras ações
+      console.log('Formulário inválido');
+    }
+  }
+  fazerLogin(){
+    console.log('fazer login')
+  }
+  voltarPagina(){
+    this.router.navigate(['/']);
+  }
 
 }
