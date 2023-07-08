7e8c043d4d8f717a7fd64dc7271c9f6571f20bfc
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-08 13:35:38 -0300

    fix: pages changed in app module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,18 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { WebPageComponent } from './web-page/web-page.component';
+
+import { PageComponent } from './Interfaces/page/page.component';
+import { RegisterComponent } from './Interfaces/register/register.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    WebPageComponent
+    PageComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
