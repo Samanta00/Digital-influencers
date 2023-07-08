c5d39d345219bd74b9f5584efc1d21a948d56acc
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-08 12:22:50 -0300

    fix: changed app module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { WebPageComponent } from './web-page/web-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    WebPageComponent
   ],
   imports: [
     BrowserModule,
