3157db062fdb871bedd486757ddfcf68498a7782
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-08 13:33:34 -0300

    feat: routers pages created

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './Interfaces/page/page.component';
+import { RegisterComponent } from './Interfaces/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:PageComponent},
+  {path:'register', component:RegisterComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
