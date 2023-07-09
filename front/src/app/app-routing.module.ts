c6ef80bea8b0fe503f7e3be2c1b3afdea671a65a
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-09 13:10:07 -0300

    fix: router dashboard deleted

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageComponent } from './Interfaces/page/page.component';
 import { RegisterComponent } from './Interfaces/register/register.component';
 import { LoginComponent } from './Interfaces/login/login.component';
-import { DashboardComponent } from './Interfaces/dashboard/dashboard.component';
+
 
 const routes: Routes = [
   {path:'',component:PageComponent},
   {path:'register', component:RegisterComponent},
   {path:'login', component:LoginComponent},
-  {path:'dashboard', component:DashboardComponent}
+  
   
 ];
 
