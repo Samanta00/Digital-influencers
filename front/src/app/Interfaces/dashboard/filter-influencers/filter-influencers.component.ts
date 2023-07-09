69e3bc6bcd8c224317fe8032d2966be66c78b649
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-09 12:32:40 -0300

    feat: created filter to filter influencers

---
+++ front/src/app/Interfaces/dashboard/filter-influencers/filter-influencers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-influencers',
+  templateUrl: './filter-influencers.component.html',
+  styleUrls: ['./filter-influencers.component.css']
+})
+export class FilterInfluencersComponent {
+
+}
