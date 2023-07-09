74fa2e510e2b4ee6b0a3f582288bde97f41a948f
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-09 13:15:24 -0300

    feat: created module to list influencers

---
+++ front/src/app/Interfaces/dashboard/list/list-influencers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-influencers',
+  templateUrl: './list-influencers.component.html',
+  styleUrls: ['./list-influencers.component.css']
+})
+export class ListInfluencersComponent {
+
+}
