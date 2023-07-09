7a6f8689b3d738fa31342343ef8dfa3a34c566e6
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-09 13:13:48 -0300

    feat: created module to edit influencers

---
+++ front/src/app/Interfaces/dashboard/edit/edit-influencers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-influencers',
+  templateUrl: './edit-influencers.component.html',
+  styleUrls: ['./edit-influencers.component.css']
+})
+export class EditInfluencersComponent {
+
+}
