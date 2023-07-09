4f819e69224ebf06ff32741925aff89c421eaf44
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-09 16:35:41 -0300

    feat: created function to list influencers

--- front/src/app/Interfaces/dashboard/list/list-influencers.component.ts
+++ front/src/app/Interfaces/dashboard/list/list-influencers.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ServicesService } from 'src/app/services/services.service';
 
 @Component({
   selector: 'app-list-influencers',
   templateUrl: './list-influencers.component.html',
   styleUrls: ['./list-influencers.component.css']
 })
-export class ListInfluencersComponent {
+export class ListInfluencersComponent implements OnInit {
+  filtroForm: FormGroup;
+  registros: any[] = [];
 
+  constructor(private formBuilder: FormBuilder, private dataService: ServicesService) {
+    this.filtroForm = this.formBuilder.group({
+      nome: [''],
+      categoria: [''],
+      inscritos: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.getRegistros();
+  }
+
+  getRegistros() {
+    this.dataService.getAllPeople().subscribe(
+      (data: any[]) => {
+        this.registros = data;
+      },
+      (error: any) => {
+        console.error('Erro ao obter registros:', error);
+      }
+    );
+  }
+
+  filtrarRegistros() {
+    const filtro = this.filtroForm.value;
+    // Implemente a lógica de filtro aqui, usando os valores do formulário
+    // Exemplo:
+    const nomeFiltro = filtro.nome;
+    const categoriaFiltro = filtro.categoria;
+    const inscritosFiltro = filtro.inscritos;
+
+    // Chame o serviço de filtro passando os parâmetros adequados
+    this.dataService.filtrarRegistros(nomeFiltro, categoriaFiltro, inscritosFiltro).subscribe(
+      (data: any[]) => {
+        this.registros = data;
+      },
+      (error: any) => {
+        console.error('Erro ao filtrar registros:', error);
+      }
+    );
+  }
+
+  resetFiltro() {
+    this.filtroForm.reset();
+    this.getRegistros();
+  }
 }
