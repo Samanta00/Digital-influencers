7dc8a7ce986f2aca8f84c8dfca32714fcd3e8971
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-09 13:29:59 -0300

    fix: added router dashboard

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageComponent } from './Interfaces/page/page.component';
 import { RegisterComponent } from './Interfaces/register/register.component';
 import { LoginComponent } from './Interfaces/login/login.component';
+import { DashboardtemplateComponent } from './Interfaces/dashboard/menu/dashboardtemplate.component';
 
 
 const routes: Routes = [
   {path:'',component:PageComponent},
   {path:'register', component:RegisterComponent},
   {path:'login', component:LoginComponent},
+  {path:'dashboard', component:DashboardtemplateComponent}
   
   
 ];
