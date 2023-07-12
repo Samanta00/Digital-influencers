8341e79265e67dbdccbae50c11d172d8836c6f93
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-12 19:55:04 -0300

    fix: added new functions to new user

--- front/src/app/services/services.service.ts
+++ front/src/app/services/services.service.ts
@@ -1,26 +1,43 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
-  private SERVER_URL = 'http://localhost:8081/auth';
-  private token: string = '';
+  private SERVER_URL_AUTH = 'http://localhost:8081/auth';
+  private SERVER_URL = 'http://localhost:8081';
 
-setToken(token: string) {
-  this.token = token;
-}
+  private token: string = '';
+  private nome: string='';
 
 
   constructor(private http: HttpClient) { }
 
+  setToken(token: string) {
+    this.token = token;
+  }
+  // Método para recuperar o token
+  getToken(): string {
+    return this.token;
+  }
+
+  // Método para definir o email
+  setEmail(nome: string): void {
+    this.nome = nome;
+  }
+
+  // Método para recuperar o email
+  getEmail(): string {
+    return this.nome;
+  }
   // Outros métodos do serviço...
 
   // Rota para visualizar todas as pessoas cadastradas
   public getAllPeople(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.SERVER_URL}/customers`);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.token);
+    return this.http.get<any[]>(`${this.SERVER_URL}/customers/`, { headers });
   }
 
   // Rota para visualizar uma pessoa cadastrada por ID
@@ -30,8 +47,15 @@ setToken(token: string) {
 
   // Rota para cadastrar uma pessoa
   public createPerson(personData: any): Observable<any> {
-    return this.http.post<any>(`${this.SERVER_URL}/customers`, personData);
+    return this.http.post<any>(`${this.SERVER_URL}/customers/cadastro`, personData)
+      .pipe(
+        catchError((error: any) => {
+          console.error('Erro ao cadastrar pessoa:', error);
+          throw error; // Lança o erro novamente para ser tratado pelo componente que chamou a função
+        })
+      );
   }
+  
 
   // Rota para atualizar dados de uma pessoa
   public updatePersonById(id: string, updatedData: any): Observable<any> {
@@ -45,18 +69,31 @@ setToken(token: string) {
 
   // Rota para fazer login e obter token de autenticação
   public login(credentials: any): Observable<any> {
-    return this.http.post<any>(`${this.SERVER_URL}/login`, credentials);
+    return this.http.post<any>(`${this.SERVER_URL_AUTH}/login`, credentials);
   }
 
-  filtrarRegistros(nome: string, categoria: string, inscritos: number): Observable<any[]> {
-    // Implemente a lógica de filtragem aqui e faça a chamada HTTP adequada
-    // Retorne os resultados filtrados como um Observable<any[]>
-    // Exemplo:
-    const params = { nome, categoria, inscritos };
-    return this.http.get<any[]>(`${this.SERVER_URL}/customers/filtro`, { params });
+  filtrarRegistros(filtro: any): Observable<any[]> {
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.token);
+    return this.http.get<any[]>(`${this.SERVER_URL}/customers/filtro`, { headers, params: filtro });
   }
+  
   isAuthenticated(): boolean {
     return this.token !== ''; // Verifica se o token está presente
+    
   }
+  getNome(): string {
+    // Lógica para obter o nome de usuário autenticado
+    return 'ellen.samanta@outlook.com'; // Retorne o nome de usuário real ou um valor padrão
+  }
+  
+  getSenha(): string {
+    // Lógica para obter a senha do usuário autenticado
+    return '123'; // Retorne a senha real ou um valor padrão
+  }
+  isUserAuthorized(email: string, senha: string): boolean {
+    return (email === 'ellen.samanta@outlook.com' && senha === '123');
+  }
+    
+  
   
 }
